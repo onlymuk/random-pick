95770017de767403c2c40b551f75de46c08bf89e
Hyung Kyu <onlymuk@naver.com>
2023-06-01 18:26:13 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -25,15 +25,22 @@ function App() {
   useEffect(() => {
     setCards(datas);
   }, []);
-  console.log(cards);
-  console.log(pickedCards);
 
+  const result = pickedCards.map((pickedCard) => {
+    return (
+      <BusinessCard
+        info={pickedCard}
+        key={pickedCard.phoneNumber}
+      ></BusinessCard>
+    );
+  });
   return (
     <div className="App">
       {cards.length > 0 && <button onClick={draw}>추첨하기</button>}
-      {pickedCards.length > 0 && (
-        <BusinessCard info={pickedCards[pickedCards.length - 1]} />
-      )}
+      {pickedCards.length > 0 &&
+        pickedCards.map((pickedCard) => {
+          return <BusinessCard info={pickedCard}></BusinessCard>;
+        })}
     </div>
   );
 }
